4f5a7d18f4f46a894ef0aa9fbb80c85bb5b5bc00
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-07-24 14:31:40 +0300

    Настройка папок

---
+++ signup.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create new account</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:100,300,400,500,600,700,800,900&amp;subset=cyrillic" rel="stylesheet">
+</head>
+<body>
+
+<script src="assets/js/jquery.min.js"></script>
+</body>
+</html>
