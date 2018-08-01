bed11b3569f6dfb50eeb15fe44f47a80b575e960
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-08-01 14:43:14 +0300

    01.08.2018

--- signup.php
+++ signup.php
@@ -11,7 +11,33 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat+Alternates:100,300,400,500,600,700,800,900&amp;subset=cyrillic" rel="stylesheet">
 </head>
 <body>
-
+    <div class="signup-container">
+        <div class="account-left">
+            <h1>Account left</h1>
+        </div>
+        <div class="account-right">
+            <div class="form-area">
+                <form action="" method="post">
+                    <div class="group">
+                        <input type="text" name="full_name" class="control" placeholder="Enter Full name...">
+                    </div>
+                    <div class="group">
+                        <input type="email" name="email" class="control" placeholder="Enter Email...">
+                    </div>
+                    <div class="group">
+                        <input type="password" name="password" class="control" placeholder="Create Password...">
+                    </div>
+                    <div class="group">
+                        <label for="file" id="file-label">Choose image</label>
+                        <input type="file" name="img" class="file" id="file">
+                    </div>
+                    <div class="group">
+                        <input type="submit" name="signup" class="btn signup-btn" value="Create account">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 <script src="assets/js/jquery.min.js"></script>
 </body>
 </html>
